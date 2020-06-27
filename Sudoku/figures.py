7e2c1a4c92b238245c7aa26a72dc83134d91ccb2
Mateusz Litwin <m_litwin@wp.pl>
2020-06-27 18:05:29 +0200

    In Progress

---
+++ Sudoku/figures.py
@@ -0,0 +1,104 @@
+import pygame as p
+from colors import *
+
+WIDTH = 900
+HEIGHT = 1000
+screen = p.display.set_mode((WIDTH, HEIGHT))
+screen.fill(white)
+p.font.init()
+font = p.font.SysFont('Arial', 40)
+
+example = (6, 3, 0, 0, 2, 0, 0, 0, 9,
+		   0, 4, 0, 5, 3, 1, 0, 0, 2,
+		   0, 7, 5, 0, 4, 9, 0, 3, 1,
+		   8, 0, 0, 4, 0, 6, 1, 0, 0,
+		   0, 0, 0, 2, 1, 0, 3, 9, 6,
+		   0, 0, 0, 7, 0, 3, 2, 0, 4,
+		   3, 8, 7, 0, 0, 0, 4, 0, 0,
+		   4, 0, 2, 1, 0, 0, 0, 6, 3,
+		   0, 0, 0, 0, 7, 0, 0, 0, 0)
+
+
+class square:
+	def __init__(self, x, y, scr, text=0):
+		self.x = x
+		self.y = y
+		self.rect = p.Rect(x, y, 100, 100)
+		if text == 0:
+			self.text_val = ''
+		else:
+			self.text_val = str(text)
+		self.text = font.render(self.text_val, True, black)
+		self.screen = scr
+		self.focused = False
+		self.value = text
+
+	def Draw(self):
+		p.draw.rect(self.screen, black, self.rect, 1)
+		text_rect = self.text.get_rect(center=(self.x+50, self.y+50))
+		self.screen.blit(self.text, text_rect)
+
+	def UpdateText(self, text):
+		p.draw.rect(self.screen, white, self.rect, 0)
+		self.text = font.render(str(text), True, black)
+		p.draw.rect(self.screen, green, self.rect, 5)
+		self.value = text
+
+	def CheckIfMouseOver(self):
+		# noinspection PyArgumentList
+		return self.rect.collidepoint(p.mouse.get_pos())
+
+	def Click(self):
+		for sq in sqaures:
+			p.draw.rect(self.screen, white, sq.rect, 5)
+			sq.focused = False
+		p.draw.rect(self.screen, green, self.rect, 5)
+		self.focused = True
+
+
+class line:
+	def __init__(self, x, y, horizontal):
+		self.x = x
+		self.y = y
+		if horizontal:
+			self.width = 900
+			self.height = 4
+		else:
+			self.width = 4
+			self.height = 900
+		self.rect = p.Rect(x, y, self.width, self.height)
+
+	def Draw(self):
+		p.draw.rect(screen, black, self.rect)
+
+
+# INITIALIZE SQUARES
+sqaures = []
+for i in range(9):
+	for j in range(9):
+		sqaures.append(square(j*100, i*100, screen, example[i*9 + j]))
+
+# INITIALIZE LINES
+lines = []
+for i in range(2):
+	for j in range(1, 3):
+		if i == 0:
+			lines.append(line(j*300, i, False))
+		else:
+			lines.append(line(i-1, j*300, True))
+
+
+def DrawSquares():
+	for sq in sqaures:
+		sq.Draw()
+
+
+def DrawLines():
+	for ln in lines:
+		ln.Draw()
+
+
+def ChangeSquareText(text):
+	for sq in sqaures:
+		if sq.focused:
+			sq.UpdateText(text)
---
+++ Sudoku/main.py
@@ -0,0 +1,38 @@
+from figures import *
+
+p.init()
+p.display.set_caption("SUDOKU")
+
+running = True
+while running:
+	p.display.update()
+	DrawSquares()
+	DrawLines()
+	for event in p.event.get():
+		if event.type == p.QUIT:
+			running = False
+			p.quit()
+			quit()
+		elif event.type == p.MOUSEBUTTONDOWN:
+			for sq in sqaures:
+				if sq.CheckIfMouseOver():
+					sq.Click()
+		elif event.type == p.KEYDOWN:
+			if event.key == p.K_1:
+				ChangeSquareText(1)
+			elif event.key == p.K_2:
+				ChangeSquareText(2)
+			elif event.key == p.K_3:
+				ChangeSquareText(3)
+			elif event.key == p.K_4:
+				ChangeSquareText(4)
+			elif event.key == p.K_5:
+				ChangeSquareText(5)
+			elif event.key == p.K_6:
+				ChangeSquareText(6)
+			elif event.key == p.K_7:
+				ChangeSquareText(7)
+			elif event.key == p.K_8:
+				ChangeSquareText(8)
+			elif event.key == p.K_9:
+				ChangeSquareText(9)
